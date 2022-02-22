cacf24f813c08c1d47c7e6a890c9d7167951806d
Masaki Ono <mo.synthkey.0112@gmail.com>
2022-02-23 08:24:41 +0900

    show the liveset content using vue

---
+++ cui_version/modx_liveset_utility_cui.js
@@ -0,0 +1,240 @@
+// YAMAHA MODX Liveset Utility
+// Copyright © 2021 Masaki Ono. All rights reserved.
+
+'use strict';
+
+const { from } = require("core-js/core/array");
+const fs = require("fs");
+
+// see doc/x8a_structure.md for detail
+
+// these const values are only used in ModxLivesetUtility
+const NUM_BANK = 8;
+const NUM_PAGE = 16;
+const NUM_SLOT = 16;
+const SIZE_SLOT = 0x11E;
+const SIZE_PAGE = SIZE_SLOT * 16 + 0x15;
+
+class ModxLivesetUtility {
+    constructor() {
+        this.initialize();
+    }
+    
+    initialize() {
+        this.x8aData_ = null;
+        this.isDataValid_ = false;
+
+        this.bankAddresses_ = new Array(NUM_BANK);
+        this.bankNameAddresses_ = new Array(NUM_BANK);
+        
+        this.pageAddresses_ = new Array(NUM_BANK);
+        for (let bankIndex = 0; bankIndex < NUM_BANK; ++bankIndex) this.pageAddresses_[bankIndex] = new Array(NUM_PAGE);
+
+        this.slotAddresses_ = new Array(NUM_BANK);
+        for (let bankIndex = 0; bankIndex < NUM_BANK; ++bankIndex) {
+            this.slotAddresses_[bankIndex] = new Array(NUM_PAGE);
+            for (let pageIndex = 0; pageIndex < NUM_PAGE; ++pageIndex) {
+                this.slotAddresses_[bankIndex][pageIndex] = new Array(NUM_SLOT);
+            }
+        }
+
+        this.numOfBanks_ = 0;
+    }
+
+    setX8ADataBuffer(x8aDataBuffer) {
+        console.log(Object.prototype.toString.call(x8aDataBuffer));
+
+        this.initialize();
+
+        this.x8aDataBuffer_ = x8aDataBuffer;
+
+        // Search DLST from header
+        const header_buffer = x8aDataBuffer.subarray(0, 0x110);
+        let dlst_addr_index = header_buffer.indexOf(Buffer.from('DLST'));
+        console.log(Buffer.from('YAMA'));
+        if (dlst_addr_index < 0) throw('DLST not found in the header');
+        dlst_addr_index += 0x04;
+        const dlst_addr = x8aDataBuffer.subarray(dlst_addr_index, dlst_addr_index + 4).readInt32BE();
+
+        // Mark addresses
+        this.numOfBanks_ = x8aDataBuffer.subarray(dlst_addr + 4, dlst_addr + 4 + 2).readInt16BE();
+        if (NUM_BANK < this.numOfBanks_) throw("Invalid number of banks")
+        console.log("Num of banks : " + this.numOfBanks_);
+
+        let readIndex = dlst_addr + 4 /* DLST */ + 8 /* ? */;
+        for (let bankIndex = 0; bankIndex < NUM_BANK; ++bankIndex) {
+            this.bankAddresses_[bankIndex] = readIndex;
+
+            // Skip "Data" + ?
+            readIndex += 4 /* Data */ + 8 /* ? */;
+
+            this.bankNameAddresses_[bankIndex] = readIndex;
+            readIndex += 0x15;
+
+            for (let pageIndex = 0; pageIndex < NUM_PAGE; ++pageIndex) {
+                this.pageAddresses_[bankIndex][pageIndex] = readIndex;
+                readIndex += 0x15;
+                
+                for (let slotIndex = 0; slotIndex < NUM_SLOT; ++slotIndex) {
+                    this.slotAddresses_[bankIndex][pageIndex][slotIndex] = readIndex;
+                    readIndex += SIZE_SLOT;
+                }
+            }
+        }
+
+        // ELST (Currently not used)
+        /*
+        let elst_addr_index = header_buffer.indexOf(Buffer.from('ELST'));
+        if (elst_addr_index < 0) throw('ELST not found');
+        elst_addr_index += 0x04;
+
+        const elst_addr = x8aDataBuffer.subarray(elst_addr_index, elst_addr_index + 4).readInt32BE();
+
+        readIndex = elst_addr + 0x0c;
+        for (let bankIndex = 0; bankIndex < this.numOfBanks_; ++bankIndex) {
+            readIndex += 0x04; // Skip "Entr"
+
+            const size = x8aDataBuffer.subarray(readIndex, readIndex + 4).readInt32BE();
+            readIndex += 0x04;
+            const name_length = size - 0x16 - 0x02;
+
+            console.log(x8aDataBuffer.subarray(readIndex + 0x14, readIndex + 0x16));
+            readIndex += 0x16;
+
+            console.log(this.createValidStringFromNullTerminatedBufferIndex(readIndex, name_length + 0x02));
+            readIndex += name_length + 0x02;
+        }
+        */
+        
+        this.isDataValid_ = true;
+    }
+
+    getX8ADataBuffer() {
+        return this.x8aDataBuffer_;
+    }
+
+    getNumOfBanks() {
+        if (!this.isDataValid_) throw("Invalid data");
+
+        return this.numOfBanks_;
+    }
+
+    getBankName(bankIndex) {
+        if (!this.isDataValid_)            throw("Invalid data");
+        if (this.numOfBanks_ <= bankIndex) throw("Bank index : out of range");
+
+        return this.createValidStringFromNullTerminatedBufferIndex(this.bankNameAddresses_[bankIndex], 0x15);
+    }
+
+    getPageName(bankIndex, pageIndex) {
+        if (!this.isDataValid_)            throw("Invalid data");
+        if (this.numOfBanks_ <= bankIndex) throw("Bank index : out of range");
+        if (NUM_PAGE <= pageIndex)         throw("Page index : out of range");
+
+        return this.createValidStringFromNullTerminatedBufferIndex(this.pageAddresses_[bankIndex][pageIndex], 0x15);
+    }
+
+    getSlotComment(bankIndex, pageIndex, slotIndex) {
+        if (!this.isDataValid_)            throw("Invalid data");
+        if (this.numOfBanks_ <= bankIndex) throw("Bank index : out of range");
+        if (NUM_PAGE <= pageIndex)         throw("Page index : out of range");
+        if (NUM_SLOT <= slotIndex)         throw("Slot index : out of range");
+
+        return this.createValidStringFromNullTerminatedBufferIndex(this.slotAddresses_[bankIndex][pageIndex][slotIndex], 0x15);
+    }
+
+    copyAndPastePage(fromBankIndex, fromPageIndex, toBankIndex, toPageIndex) {
+        const readIndex  = this.pageAddresses_[fromBankIndex][fromPageIndex];
+        const writeIndex = this.pageAddresses_[toBankIndex][toPageIndex];
+
+        for (let index = 0; index < SIZE_PAGE; ++index) {
+            this.x8aDataBuffer_[writeIndex + index] = this.x8aDataBuffer_[readIndex + index];
+        }
+    }
+
+    copyAndPasteAllPages(fromBankIndex, toBankIndex) {
+        for (let pageIndex = 0; pageIndex < NUM_PAGE; ++pageIndex) {
+            this.copyAndPastePage(fromBankIndex, pageIndex, toBankIndex, pageIndex);
+        }
+    }
+
+    swapPage(bankIndexA, pageIndexA, bankIndexB, pageIndexB) {
+        const indexA =  this.pageAddresses_[bankIndexA][pageIndexA];
+        const indexB =  this.pageAddresses_[bankIndexB][pageIndexB];
+
+        for (let index = 0; index < SIZE_PAGE; ++index) {
+            const aData = this.x8aDataBuffer_[indexA + index];
+            this.x8aDataBuffer_[indexA + index] = this.x8aDataBuffer_[indexB + index];
+            this.x8aDataBuffer_[indexB + index] = aData;
+        }
+    }
+
+    swapAllPages(bankIndexA, bankIndexB) {
+        for (let pageIndex = 0; pageIndex < NUM_PAGE; ++pageIndex) {
+            this.swapPage(bankIndexA, pageIndex, bankIndexB, pageIndex);
+        }
+    }
+
+    print() {
+        // for debug
+        for (let bankIndex = 0; bankIndex < this.numOfBanks_; ++bankIndex) {
+            console.log(`Bank${bankIndex + 1} : ${this.getBankName(bankIndex)}`);
+
+            for (let pageIndex = 0; pageIndex < NUM_PAGE; ++pageIndex) {
+                console.log(`  Page${pageIndex + 1} : ${this.getPageName(bankIndex, pageIndex)}`);
+                for (let slotIndex = 0; slotIndex < NUM_SLOT; ++slotIndex) {
+                    const comment = this.getSlotComment(bankIndex, pageIndex, slotIndex);
+                    if (comment != "") console.log(`    Slot${slotIndex + 1} : ${comment}`);
+                }
+            }
+        }
+    }
+
+    createValidStringFromNullTerminatedBufferIndex(index, size) {
+        const buffer = this.x8aDataBuffer_.subarray(index, index + size);
+        return buffer.subarray(0, buffer.indexOf(0)).toString();
+    }
+}
+
+function printLivesetContents(fileName) {
+    try {
+        let utility = new ModxLivesetUtility();
+        const buffer = fs.readFileSync(fileName);
+        console.log(Object.prototype.toString.call(buffer));
+        utility.setX8ADataBuffer(buffer);
+
+        /*
+        utility.print();
+
+        console.log("---");
+
+        utility.swapAllPages(2, 5);
+
+        utility.print();
+
+        const modifiedDataBuffer = utility.getX8ADataBuffer();
+        const writeFileName = fileName.substring(0, fileName.lastIndexOf(".")) + "_mod.x8a"
+        fs.writeFileSync(writeFileName, modifiedDataBuffer);
+        */
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const indexOfArray = (fromArray, searchArray) => {
+    let fromIndex = 0;
+    while (fromIndex + searchArray.length < fromArray.length) {
+        let index = fromArray.indexOf(searchArray[0], fromIndex);
+        if (index == -1) return -1;
+        const subarray = fromArray.subarray(index, index + searchArray.length);
+        if (subarray.toString() == searchArray.toString()) return index;
+        fromIndex = index + 1;
+    }
+};
+
+/*
+
+if (process.argv.length <= 2) throw("Few args");
+console.log(process.argv[2]);
+printLivesetContents(process.argv[2]);
+*/
--- src/components/ModxSetlistUtility.vue
+++ src/components/ModxSetlistUtility.vue
@@ -1,8 +1,21 @@
-<template>
-<div id='ModxLivesetUtility'>
-  <h1>MODX LIVESET Utility</h1>
-  <input type="file" @change="uploadFile"/>
-</div>
+<template> 
+    <div id='ModxLivesetUtility'>
+        <h1> MODX LIVESET Utility </h1> 
+        <input type = "file" @change = "uploadFile"/><br>
+
+        <div v-for="bank in banks" :key="bank.index">
+            <b>Bank {{bank.index}} : {{ bank.name }}</b>
+            <div v-for="page in bank.pages" :key="page.index">
+                Page {{page.index}} : {{ page.name }}
+                <div v-for="slot in page.slots" :key="slot.index">
+                    {{ slot.comment }}
+                </div>
+            </div>
+            <hr>
+        </div>
+        <b-button variant="primary">bootstrap</b-button>
+
+    </div> 
 </template>
 
 <script>
@@ -10,25 +23,60 @@ import Utility from "../modx_liveset_utility"
 import { Buffer } from 'buffer';
 
 export default {
-  name: 'ModxLivesetUtility',
-  setup() {
-    let utility = new Utility.ModxLivesetUtility();
-    
-    const uploadFile = (file) => {
-      const fileObject = file.target.files[0];
-      
-      let fileReader = new FileReader();
-      fileReader.readAsArrayBuffer(fileObject);
-      fileReader.onload = () => {
-        utility.setX8ADataBuffer(Buffer.from(fileReader.result));
-        utility.print();
-      };
-    };
+    name: 'ModxLivesetUtility',
+    data() {
+        return {
+            message: "",
+            banks : [],
+        };
+    },
+    created() {
+        this.utility = new Utility.ModxLivesetUtility();
+        this.banks = new Array(8);
+        for (let bankIndex = 0; bankIndex < 8; ++bankIndex) {
+            this.banks[bankIndex] = { name: "", index:bankIndex + 1, pages:new Array(16) };
+            for (let pageIndex = 0; pageIndex < 16; ++pageIndex) {
+                this.banks[bankIndex].pages[pageIndex] = { name:"", index: pageIndex + 1, slots:new Array(16) };
+                for (let slotIndex = 0; slotIndex < 16; ++slotIndex) {
+                    this.banks[bankIndex].pages[pageIndex].slots[slotIndex] = { index: slotIndex + 1, comment: "" };
+                }
+            }
+        }
+    },
+    methods: {
+        uploadFile(file) {
+            const fileObject = file.target.files[0];
 
-    return { uploadFile };
-  }
-}
+            let fileReader = new FileReader();
+            fileReader.readAsArrayBuffer(fileObject);
+
+            fileReader.onload = () => {
+                this.utility.setX8ADataBuffer(Buffer.from(fileReader.result));
+
+                // debug
+                const numOfBanks = this.utility.getNumOfBanks();
+                const numOfPages = 16;
+                this.message = "";
+                for (let bankIndex = 0; bankIndex < numOfBanks; ++bankIndex) {
+                    const bankName = this.utility.getBankName(bankIndex);
+                    this.banks[bankIndex].name = bankName;
+                    for (let pageIndex = 0; pageIndex < numOfPages; ++pageIndex) {
+                        const pageName = this.utility.getPageName(bankIndex, pageIndex);
+                        this.banks[bankIndex].pages[pageIndex].name = pageName;
+                        for (let slotIndex = 0; slotIndex < 16; ++slotIndex) {
+                            this.banks[bankIndex].pages[pageIndex].slots[slotIndex].comment = this.utility.getSlotComment(bankIndex, pageIndex, slotIndex);
+                        }
+                    }
+                }
+            };
+        }
+    }
+};
 </script>
 
 <style>
+div {
+    text-align: left;
+}
+
 </style>
