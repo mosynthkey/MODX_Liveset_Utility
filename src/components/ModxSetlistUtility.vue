4b19e2e7904ba723206bcab6c4f7ce2add976ed3
Masaki Ono <mo.synthkey.0112@gmail.com>
2022-02-24 23:17:34 +0900

    add the header

--- src/components/ModxSetlistUtility.vue
+++ src/components/ModxSetlistUtility.vue
@@ -1,34 +1,57 @@
 <template> 
-    <div id='ModxLivesetUtility'>
-        MODX LIVESET Utility
-        <q-file v-model="model" label="Click to pick .x8a file" @input="uploadFile" style="max-width: 300px"/>
+    <div id="q-app">
+        <q-layout class="shadow-2 rounded-borders">
+            <q-header>
+                <q-toolbar>
+                    <q-btn flat round dense icon="menu" class="q-mr-sm" ></q-btn>
         
+                    <q-toolbar-title>MODX LIVESET Utility</q-toolbar-title>
 
-        <div v-for="bank in banks" :key="bank.index">
-            <b>Bank {{bank.index}} : {{ bank.name }}</b>
-            <div v-for="page in bank.pages" :key="page.index">
-                Page {{page.index}} : {{ page.name }}
-                <div v-for="slot in page.slots" :key="slot.index">
-                    {{ slot.comment }}
-                </div>
-            </div>
-            <hr>
-        </div>
-    </div> 
+                    <q-file label="Upload" @input="uploadFile">
+                        <template v-slot:prepend>
+                        <q-icon name="cloud_upload" />
+                        </template>
+                    </q-file>
+
+                    <q-btn label="Download" @click="download()" no-caps />
+                    
+        
+                </q-toolbar>
+            </q-header>
+  
+            <q-page-container>
+                <q-page>
+                    <div v-if="isLoaded">
+                        <div v-for="bank in banks" :key="bank.index">
+                            <b>Bank {{bank.index}} : {{ bank.name }}</b>
+                            <div v-for="page in bank.pages" :key="page.index">
+                                Page {{page.index}} : {{ page.name }}
+                                <div v-for="slot in page.slots" :key="slot.index">
+                                    {{ slot.comment }}
+                                </div>
+                            </div>
+                            <hr>
+                        </div>
+                    </div>
+                    <div v-else>
+                        Please load file using a button on header
+                    </div>
+                </q-page>
+            </q-page-container>
+        </q-layout>      
+    </div>  
 </template>
 
 <script>
 import Utility from "../modx_liveset_utility"
 import { Buffer } from 'buffer';
-import { ref } from 'vue';
+import { exportFile } from 'quasar'
 
 export default {
-    name: 'ModxLivesetUtility',
     data() {
         return {
-            message: "",
+            isLoaded: false,
             banks : [],
-            model: ref(null)
         };
     },
     created() {
@@ -46,40 +69,49 @@ export default {
     },
     methods: {
         uploadFile(file) {
-            console.log(this.model);
-
             const fileObject = file.target.files[0];
 
             let fileReader = new FileReader();
             fileReader.readAsArrayBuffer(fileObject);
 
             fileReader.onload = () => {
-                this.utility.setX8ADataBuffer(Buffer.from(fileReader.result));
+                try {
+                    this.utility.setX8ADataBuffer(Buffer.from(fileReader.result));
 
-                // debug
-                const numOfBanks = this.utility.getNumOfBanks();
-                const numOfPages = 16;
-                this.message = "";
-                for (let bankIndex = 0; bankIndex < numOfBanks; ++bankIndex) {
-                    const bankName = this.utility.getBankName(bankIndex);
-                    this.banks[bankIndex].name = bankName;
-                    for (let pageIndex = 0; pageIndex < numOfPages; ++pageIndex) {
-                        const pageName = this.utility.getPageName(bankIndex, pageIndex);
-                        this.banks[bankIndex].pages[pageIndex].name = pageName;
-                        for (let slotIndex = 0; slotIndex < 16; ++slotIndex) {
-                            this.banks[bankIndex].pages[pageIndex].slots[slotIndex].comment = this.utility.getSlotComment(bankIndex, pageIndex, slotIndex);
+                    const numOfBanks = this.utility.getNumOfBanks();
+                    const numOfPages = 16;
+                    this.message = "";
+                    for (let bankIndex = 0; bankIndex < numOfBanks; ++bankIndex) {
+                        const bankName = this.utility.getBankName(bankIndex);
+                        this.banks[bankIndex].name = bankName;
+                        for (let pageIndex = 0; pageIndex < numOfPages; ++pageIndex) {
+                            const pageName = this.utility.getPageName(bankIndex, pageIndex);
+                            this.banks[bankIndex].pages[pageIndex].name = pageName;
+                            for (let slotIndex = 0; slotIndex < 16; ++slotIndex) {
+                                this.banks[bankIndex].pages[pageIndex].slots[slotIndex].comment = this.utility.getSlotComment(bankIndex, pageIndex, slotIndex);
+                            }
                         }
                     }
+
+                    this.isLoaded = true;
+                } catch (e) {
+                    console.log(e);
                 }
             };
+        },
+        download() {
+            const x8aDataBuffer = this.utility.getX8ADataBuffer();
+            const status = exportFile('Backup.x8a', x8aDataBuffer);
+            if (status) {
+                console.log("Succeed");
+            } else {
+                console.log("Failed " + status);
+            }
         }
     }
 };
 </script>
 
 <style>
-div {
-    text-align: left;
-}
 
 </style>
